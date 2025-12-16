d86b6fa65a8c795a8f9d0d6dd0c5b3eadf044566
Metall1st <93581822+Dement1cs@users.noreply.github.com>
2025-12-16 21:24:39 +0000

    -__-

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
 bundle install
+bin/rails assets:precompile
+bin/rails assets:clean
+bin/rails db:migrate
 
-# поставить JS зависимости (для jsbundling-rails)
-if [ -f yarn.lock ]; then
-  yarn install --frozen-lockfile
-elif [ -f package-lock.json ]; then
-  npm ci
-elif [ -f bun.lockb ]; then
-  bun install --frozen-lockfile
-fi
-
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 

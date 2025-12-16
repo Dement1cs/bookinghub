5b8625a092b6c068002d587211767455ed631618
Metall1st <93581822+Dement1cs@users.noreply.github.com>
2025-12-16 08:44:37 +0000

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,16 @@
 set -o errexit
 
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
-bin/rails db:migrate
+
+# поставить JS зависимости (для jsbundling-rails)
+if [ -f yarn.lock ]; then
+  yarn install --frozen-lockfile
+elif [ -f package-lock.json ]; then
+  npm ci
+elif [ -f bun.lockb ]; then
+  bun install --frozen-lockfile
+fi
+
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
